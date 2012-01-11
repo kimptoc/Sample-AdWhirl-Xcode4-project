b1df15a0e573ab248e82cd31ee0aec44eb5b35fd
Chris Kimpton <chris@kimptoc.net>
2012-01-11 22:21:12 +0000

    Initial Commit

---
+++ tester/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tester
+//
+//  Created by Chris Kimpton on 11/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testerAppDelegate class]));
+    }
+}
---
+++ tester/testerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  testerAppDelegate.h
+//  tester
+//
+//  Created by Chris Kimpton on 11/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
